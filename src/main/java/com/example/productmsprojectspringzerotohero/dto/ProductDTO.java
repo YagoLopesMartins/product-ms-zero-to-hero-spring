d337030a20a41d381aad6fe891b29158b2f387ea
Yago <ylm@icomp.ufam.edu.br>
2024-02-13 01:00:14 -0300

    Validation and add gradle dependencies

--- src/main/java/com/example/productmsprojectspringzerotohero/dto/ProductDTO.java
+++ src/main/java/com/example/productmsprojectspringzerotohero/dto/ProductDTO.java
@@ -1,12 +1,21 @@
 package com.example.productmsprojectspringzerotohero.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class ProductDTO {
 
+    @NotBlank
     private String name;
+
+    @Size(min = 50)
     private String description;
+
+    @Positive
     private Double price;
+
     private Boolean available;
 }
